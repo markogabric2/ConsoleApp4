ee28c3a3571548535adfcfac4955a10747a6dea9
student <student@B420-XY>
2021-03-25 17:16:55 +0100

    klasa

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Class1
+    {
+        void Poruka()
+        {
+            Console.WriteLine("Hello World!");
+        }
+       
+    }
+}
